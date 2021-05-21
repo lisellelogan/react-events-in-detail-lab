abcb7201954bbdbaea85968b1cdf3e7ce0f11483
lisellelogan <liselle.logan@yahoo.com>
2021-05-21 18:34:16 -0400

    commit tree e382712a7307e55cb0f0fca3eae9c3c23e9774a5

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,27 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+// In the components/DelayedButton.js file, create a DelayedButton React component
+
+// This component takes two props: onDelayedClick (a function), and delay (a number).
+
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+
+// If successful, the event will be logged to the console once the timeout has finished.
+
+// Hint: If you having trouble with this feature, remember event pooling in React. By the time the setTimeout fires, the event object will have already been returned to the pool. So how can we fix that?
+
+export default class DelayedButton extends Component {
+
+    handleClick = (event) => {
+        event.persist();
+        window.setTimeout(() => {
+            this.props.onDelayedClick(event);
+        }, this.props.delay);
+    }
+
+    render(){
+        return(
+            <button onClick={this.handleClick}>Delayed</button>
+        )
+    }
+}
